ef44f570cd518f6cba17a8f499861e9c25407add
moez-RT <saidi.moez.2014@gmail.com>
2020-02-19 08:52:59 +0100

    19-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'projetv1';
   activate(event) {
-    event.target.classList.add('active');
+    event.target.classList.toggle('active');
   }
 }
